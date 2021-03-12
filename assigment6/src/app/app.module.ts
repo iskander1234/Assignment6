01456e3b058b9f375c6fea0451e051ec6992197f
Iskander Dosgali <idosgali@gmail.com>
2021-03-12 23:46:52 +0600

    В app.module.ts мы вызвали Component и providers : [SharedService]

--- assigment6/src/app/app.module.ts
+++ assigment6/src/app/app.module.ts
@@ -3,16 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {Child3Component} from './Child/child3.component';
+import {SharedService} from './Service/shared.service';
+import {Child2Component} from './Child/child2.component';
+import {Child1Component} from './Child/child1.component';
+import {Parent2Component} from './Parent/parent2.component';
+import {Parent1Component} from './Parent/parent1.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Parent1Component,
+    Parent2Component,
+    Child1Component,
+    Child2Component,
+    Child3Component
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [SharedService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
